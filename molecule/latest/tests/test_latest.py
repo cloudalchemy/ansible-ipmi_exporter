73599a811eb26185929513ee272cd8140d74bc77
paulfantom <pawel@krupa.net.pl>
2020-12-10 18:16:04 +0100

    molecule: add some tests to make CI work

--- molecule/latest/tests/test_latest.py
+++ molecule/latest/tests/test_latest.py
@@ -1,5 +1,15 @@
 import os
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/systemd/system/ipmi_exporter.service"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
